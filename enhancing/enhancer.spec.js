372de2a8257cbe608062de663320b4afd38dd18f
BryanPAnderson <AndersonBryan53@gmail.com>
2021-02-01 01:53:48 -0500

    final

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,38 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe("item functions", () => {
+    it("repairs durability", () => {
+        expect(enhancer.repair({
+            name: "theItem",
+            durability: 25,
+            enhancement: 14
+        })).toEqual({
+            name: "theItem",
+            durability: 100,
+            enhancement: 14
+        })
+    })
+
+    it("succeeds", () => {
+        expect(enhancer.success({
+            name: "theItem",
+            durability: 1,
+            enhancement: 30
+        })).toEqual({
+            name: "theItem",
+            durability: 1,
+            enhancement: 30
+        })
+    })
+    it("fails", () => {
+        expect(enhancer.fail({
+            name: "theItem",
+            durability: 14,
+            enhancement: 95
+        })).toEqual({
+            name: "theItem",
+            durability: 15,
+            enhancement: 100
+        })
+    })
+})
